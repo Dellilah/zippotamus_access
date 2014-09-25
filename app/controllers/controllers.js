8319c39ea5669d9ee5b3843850795494e556d6f7
Alicja <acyganiewicz@gmail.com>
2014-09-25 14:46:15 +0200

    Wrong lat/long - clear map, not a grey rectangle

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -4,6 +4,10 @@ app.controller('ZippoController', function ($scope, $http, zippoService) {
 
 	function init() {
 		$scope.supported_countries = zippoService.getCountries();
+		mapInit();
+	}
+
+	function mapInit() {
 		$scope.map = {
 			center: {
 				latitude: 0,
@@ -34,20 +38,27 @@ app.controller('ZippoController', function ($scope, $http, zippoService) {
 		$http({method: 'GET', url: 'http://api.zippopotam.us/'+$scope.cn.abbr+'/'+$scope.zipcode}).
 			success(function(data, status, headers, config) {
 				$scope.result = data;
-				$scope.map = {
-					center: {
-						latitude: parseFloat(data.places[0].latitude),
-						longitude: parseFloat(data.places[0].longitude)
-					},
-					zoom: 10
-				};
+				var lat_c = parseFloat(data.places[0].latitude);
+				var lon_c = parseFloat(data.places[0].longitude);
+				if ( isNaN(lat_c) || isNaN(lon_c) || lat_c > 85 || lat_c < -85 || lon_c > 180 || lon_c < -180) {
+					mapInit();
+				}
+				else {
+					$scope.map = {
+						center: {
+							latitude: lat_c,
+							longitude: lon_c
+						},
+						zoom: 10
+					};
+				}
 				$scope.markers = [];
 				for (var i = data.places.length - 1; i >= 0; i--) {
 					$scope.markers.push({
 						latitude: parseFloat(data.places[i].latitude),
 						longitude: parseFloat(data.places[i].longitude)
 					});
-				}
+				};
 				$scope.cn = '';
 				$scope.zipcode = '';
 			}).
